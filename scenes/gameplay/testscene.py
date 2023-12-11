4f722b34360a2afbdc6695a682aa83252f172e1e
Corbin <Corbinchristopher1@gmail.com>
2023-12-11 15:29:15 -0700

    Update testscene.py

--- scenes/gameplay/testscene.py
+++ scenes/gameplay/testscene.py
@@ -11,19 +11,6 @@ class TestScene(GamePlaySceneState):
     def __init__(self, game) -> None:
         super().__init__(game)
         self.game = game
-
-        Mob([self.sprites, self.enemy_group], self.textures['zombie_static'], (800, -500), parameters={
-                                                                                            'block_group':self.blocks,
-                                                                                            'player':self.player,
-                                                                                            'damage': 5,
-                                                                                            'health': 2
-                                                                                            })
-        Mob([self.sprites, self.enemy_group], self.textures['skeleton_static'], (900, -500), parameters={
-                                                                                            'block_group':self.blocks,
-                                                                                            'player':self.player,
-                                                                                            'damage': 5,
-                                                                                            'health': 2
-                                                                                            })
         
         self.gen_world()
 
